4739fd46ddc82abc96bc142239cb30bf6ce10420
Samer Najjar <snajjar@cashu.com>
2018-01-18 13:07:25 +0200

    add basic test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -5,15 +5,17 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
-{
+class ExampleTest extends TestCase {
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $this->assertTrue(true);
+    public function testBasicTest() {
+        $response = $this->post('/hotel-search', ['destinationName' => 'amman']);
+
+        $response->assertStatus(200);
     }
+
 }
